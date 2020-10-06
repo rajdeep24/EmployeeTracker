d038e6f1f9a16f59745ed16fbc47b30fe302af02
Developer Raj <rajdeepkakar@gmail.com>
2020-10-06 19:09:12 -0400

    Created the mysql connection to the employeetracker_db schema and connected to the database.

--- app.js
+++ app.js
@@ -3,3 +3,22 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 const consoleTable = require("console.table");
 
+//create the connection information for the sql database
+const connection = mysql.createConnection({
+	host: "localhost",
+	// Your port; if not 3306
+	port: 3306,
+	// Your username
+	user: "root",
+	// Your password
+	password: "rajdeepkakar",
+	database: "employeetracker_db",
+});
+
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+	if (err) throw err;
+	// run the start function after the connection is made to prompt the user
+	console.log("Connected to port:");
+	start();
+});
