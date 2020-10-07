c10d5b9c59101700886b138787ed0e03a53097bd
Developer Raj <rajdeepkakar@gmail.com>
2020-10-06 20:02:59 -0400

    Added the start function that prompts the user for what action they want to take using inquirer.

--- app.js
+++ app.js
@@ -22,3 +22,49 @@ connection.connect(function (err) {
 	console.log("Connected to port:");
 	start();
 });
+
+//function which prompts the user for what action they should take
+function start() {
+	inquirer
+		.prompt({
+			name: "action",
+			type: "list",
+			message: "Would you like to do?",
+			choices: ["View ALL Employees", "View All Employees by Department", "View All Employees by Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager", "Exit"],
+		})
+		.then(function (answer) {
+			switch (answer.action) {
+				case "View ALL Employees":
+					viewAllEmployees();
+					break;
+
+				case "View All Employees by Department":
+					viewAllEmployeesByDept();
+					break;
+
+				case "View All Employees by Manager":
+					viewAllEmployeesByManager();
+					break;
+
+				case "Add Employee":
+					addEmployee();
+					break;
+
+				case "Remove Employee":
+					removeEmployee();
+					break;
+
+				case "Update Employee Role":
+					updateEmployeeRole();
+					break;
+
+				case "Update Employee Manager":
+					updateEmployeeManager();
+					break;
+
+				case "exit":
+					connection.end();
+					break;
+			}
+		});
+}
