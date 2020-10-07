0a53b9cdefa7632a248bf0a252053282a02d1655
Developer Raj <rajdeepkakar@gmail.com>
2020-10-06 23:06:48 -0400

    Updated employeeTrackerseed.sql file to include foreign keys for both the employee and role table. In addition, designated managers during the inemployee insert process.

--- db/employeeTrackerSeed.sql
+++ db/employeeTrackerSeed.sql
@@ -1,61 +1,110 @@
-DROP DATABASE IF EXISTS employeetracker_db;
+DROP DATABASE IF EXISTS employees_DB;
 
-CREATE DATABASE employeetracker_db;
+CREATE DATABASE employees_DB;
 
-USE employeetracker_db;
+USE employees_DB;
 
+----- Create Department table -----
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
-    department_name VARCHAR(30) NOT NULL,
+    name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
+----- Create Role Table -----
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(8,2),
-    department_id INT NULL,
-    PRIMARY KEY (id)
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
+----- Create Employee Table -----
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NULL,
-    manager_id INT NULL,
-    PRIMARY KEY(id)
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
 );
-INSERT INTO department (department_name) VALUES ("CEO");
-INSERT INTO department (department_name) VALUES ("Sales");
-INSERT INTO department (department_name) VALUES ("Legal");
-INSERT INTO department (department_name) VALUES ("Finance");
-INSERT INTO department (department_name) VALUES ("Engineering");
-INSERT INTO department (department_name) VALUES ("Marketing");
 
-select * from department;
+USE employees_DB;
 
-INSERT INTO role (title,salary, department_id) VALUES ("ceo", 300000.00, 1);
-INSERT INTO role (title,salary, department_id) VALUES ("sales manager", 61000.00, 2);
-INSERT INTO role (title,salary, department_id) VALUES ("sales rep", 52000.00, 2);
-INSERT INTO role (title,salary, department_id) VALUES ("lawyer", 102000.00, 3);
-INSERT INTO role (title,salary, department_id) VALUES ("paralegal", 59000.00, 3);
-INSERT INTO role (title,salary, department_id) VALUES ("financial manager", 71000.00, 4);
-INSERT INTO role (title,salary, department_id) VALUES ("accountant", 67000.00, 4);
-INSERT INTO role (title,salary, department_id) VALUES ("lead engineer", 100000.00, 5);
-INSERT INTO role (title,salary, department_id) VALUES ("software engineer", 72000.00, 5);
+----- Department Seeds -----
 
+INSERT INTO department (id, name)
+VALUES (1, "Sales");
 
-select * from role;
+INSERT INTO department (id, name)
+VALUES (2, "Marketing");
 
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("James", "Bond", 1);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Kelly", "Smith", 2);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Tina", "Harris", 3);
-INSERT INTO employee(first_name, last_name, role_id) VALUES ("Daffy", "Duck", 4);
-INSERT INTO employee(first_name, last_name, role_id) VALUES ("Bugs", "Bunny", 5);
-INSERT INTO employee(first_name, last_name, role_id) VALUES ("Harry", "Potter", 6);
-INSERT INTO employee(first_name, last_name, role_id) VALUES ("Peter", "Pan", 7);
-INSERT INTO employee(first_name, last_name, role_id) VALUES ("Tony", "Starks", 8);
-INSERT INTO employee(first_name, last_name, role_id) VALUES ("Porky", "Pig", 9);
+INSERT INTO department (id, name)
+VALUES (3, "Human Resources");
+
+INSERT INTO department (id, name)
+VALUES (4, "Finance");
+
+----- Role Seeds -----
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, "Sales Associate", 42000, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (2, "Sales Lead", 60000, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (3, "Account Manager", 75000, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (4, "Marketing Manager", 65000, 2);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (5, "HR Recruiter", 70000, 3);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (6, "HR Manager", 100000, 3);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (7, "Lead Accountant", 80000, 4);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (8, "Accountant", 52000, 4);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (9, "Financial Manager", 105000, 4);
+
+----- Employees Seeds -----
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (3, "Daffy", "Duck", 3, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (4, "Donald", "Duck", 4, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (6, "Lebron", "James", 6, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (9, "Jim", "Jones", 9, null);
+
+INSERT INTO employee (id,first_name, last_name, role_id, manager_id)
+VALUES ( 1, "James", "Bond", 1, 3);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (2, "Kim", "Kardashian", 2, 3);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (5, "Porky", "Pig", 5, 6);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (7, "Gilbert", "Arenas", 7, 9);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (8, "Tom", "Brady", 8, 9);
 
 select * from employee;
